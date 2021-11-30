80e95ac7d504c8bffa70aa92d57e7c7e31b383a8
wblazej <blazej.wrzosok@gmail.com>
2021-11-30 11:29:12 +0100

    dark and light theme switch added

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import './styles/index.scss';
+import { Toaster } from 'react-hot-toast';
+import './styles/themeSwitch.scss';
+import Home from './pages/Home';
+import Create from './pages/Create';
+import Game from './pages/Game';
+import useLocalStorage from 'react-use-localstorage';
+
+import {
+	BrowserRouter,
+	Routes,
+	Route
+} from "react-router-dom";
+
+const App = () => {
+    const [theme, setTheme] = useLocalStorage('theme', 'dark')
+
+    document.querySelector('html')?.setAttribute("data-theme", theme)
+
+    return (
+        <div className="app">
+			<BrowserRouter>
+				<Routes>
+					<Route path='/'>
+						<Route index element={<Home/>}/>
+						<Route path='/create' element={<Create/>}/>
+						<Route path='/game/:id' element={<Game/>}/>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+
+			<Toaster
+				position="bottom-left"
+			/>
+
+			<div className="theme-switch-container">
+                <p>Dark theme</p>
+			    <div className="toggle-radio">
+                    <input type="radio" name="rdo" id="yes" checked={theme === 'dark'} onChange={() => setTheme('dark')} />
+                    <input type="radio" name="rdo" id="no" checked={theme === 'light'} onChange={() => setTheme('light')} />
+                    <div className="switch">
+                        <label htmlFor="yes">Yes</label>
+                        <label htmlFor="no">No</label>
+                        <span></span>
+                    </div>
+				</div>
+			</div>
+		</div>
+    )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import './styles/index.scss';
-import { Toaster } from 'react-hot-toast';
-
-import Home from './pages/Home';
-import Create from './pages/Create';
-import Game from './pages/Game';
-
-import {
-	BrowserRouter,
-	Routes,
-	Route
-} from "react-router-dom";
+import App from './App'
 
 ReactDOM.render(
 	<React.StrictMode>
-		<div className="app">
-			<BrowserRouter>
-				<Routes>
-					<Route path='/'>
-						<Route index element={<Home/>}/>
-						<Route path='/create' element={<Create/>}/>
-						<Route path='/game/:id' element={<Game/>}/>
-					</Route>
-				</Routes>
-			</BrowserRouter>
-
-			<Toaster
-				position="bottom-left"
-			/>
-		</div>		
+		<App/>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
